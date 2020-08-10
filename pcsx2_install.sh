330c5dc698d5bb6c295482c74dbcae4d77915a5d
zach <wz.horton@gmail.com>
2020-08-09 17:33:04 -0700

    amend secure asset

--- pcsx2_install.sh
+++ pcsx2_install.sh
@@ -3,4 +3,9 @@ sudo dpkg --add-architecture i386
 sudo add-apt-repository ppa:pcsx2-team/pcsx2-daily -y
 sudo apt update -y
 sudo apt install pcsx2-unstable -y
-git clone 
+gpg -d secure.zip.gpg > secure.zip
+rm secure.zip.gpg
+unzip secure.zip
+rm secure.zip
+mv ps2_bios/* ~/.config/PCSX2/bios
+rm ps2_bios
