52ba891a989e8090236d69fad47dd8aaea6c4d1a
zach <wz.horton@gmail.com>
2020-08-11 13:43:13 -0700

    y flags

--- pcsx2_install.sh
+++ pcsx2_install.sh
@@ -4,7 +4,6 @@ sudo add-apt-repository ppa:pcsx2-team/pcsx2-daily -y
 sudo apt update -y
 sudo apt install pcsx2-unstable -y
 gpg -d secure.zip.gpg > secure.zip
-rm secure.zip.gpg
 unzip secure.zip
 rm secure.zip
 mkdir ~/.config/PCSX2 && mkdir ~/.config/PCSX2/bios
